9bb7d22c4eefd382a7747230821e4aee6dc0519d
Kevin Grove <kevinwilsongrove@gmail.com>
2021-08-13 00:43:13 -0400

    Added useEffect to shuffle order of image array.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
 import "./App.css";
 import BackgroundSlider from "react-background-slider";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function App() {
-  
-  let images = []
-  for (let i = 1; i < 17; i++) {
-    images.push(`images/img-${i}.jpeg`)
-  }
+  const [images, setImages] = useState([])
+
+  useEffect(() => {
+      for (let i = 1; images.length < 16; i++) {
+        images.push(`images/img-${i}.jpeg`)
+      }
+      
+      var copy = [], n = images.length, i;
+
+      // While there remain elements to shuffle…
+      while (n) {
+
+        // Pick a remaining element…
+        i = Math.floor(Math.random() * images.length);
+
+        // If not already shuffled, move it to the new images.
+        if (i in images) {
+          copy.push(images[i]);
+          delete images[i];
+          n--;
+        }
+      }
+      setImages(copy)
+  }, [])
 
   return (
     <>
         <BackgroundSlider 
           id="background"
           images={images}
-          duration={8}
+          duration={2}
           transition={2}
         />
           <div id="app-container"></div>
