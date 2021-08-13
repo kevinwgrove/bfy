4f6d56bac34499a21b05742110b50286b1825d8a
Kevin Grove <kevinwilsongrove@gmail.com>
2021-08-12 23:56:43 -0400

    Installed npm react background slider. Much easier, however, less customization.

--- src/App.js
+++ src/App.js
@@ -1,47 +1,22 @@
 import "./App.css";
-// import { useEffect, useState } from "react";
+import BackgroundSlider from "react-background-slider";
 
 function App() {
-  // const [num, setNum] = useState(12)
-
-  const imgUrls = new XMLHttpRequest();
-  imgUrls.open("GET", "/images", true);
-  imgUrls.responseType = "document";
-
-  // useEffect(() => {
-  //   const getRandomNum = () => {
-  //     let newNum = Math.floor(Math.random() * (17 - 1) + 1)
-  //     if (num === newNum){
-  //       getRandomNum()
-  //     } else {
-  //       setNum(newNum)
-  //     }
-  //   };
-  //   setInterval(() => getRandomNum(), 10000)
-  // })
   
-  
-  // let img_num = `url(images/img-${num}.jpeg)`;
+  let images = []
+  for (let i = 1; i < 17; i++) {
+    images.push(`images/img-${i}.jpeg`)
+  }
 
   return (
     <>
-      {/* {num === 11 ? ( */}
-        <div
+        <BackgroundSlider 
           id="background"
-          style={{
-            backgroundImage: `url(images/img-12.jpeg)`,
-            backgroundPosition: "0 5%",
-          }}
-        ></div>
-      {/* ) : (
-          <div
-            id="background"
-            style={{
-              backgroundImage: img_num,
-            }}
-          ></div>
-      )} */}
-      <div id="app-container"></div>
+          images={images}
+          duration={8}
+          transition={2}
+        />
+          <div id="app-container"></div>
     </>
   );
 }
