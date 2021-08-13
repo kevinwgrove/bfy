944991b66155ab95e21fcef7c2cedb9020470915
Kevin Grove <kevinwilsongrove@gmail.com>
2021-08-13 01:27:56 -0400

    Got rid of useState and useEffect, didn't need them

--- src/App.js
+++ src/App.js
@@ -1,35 +1,14 @@
 import "./App.css";
 import BackgroundSlider from "react-background-slider";
-import { useState } from "react";
-import { useEffect } from "react";
 
 function App() {
-  const [images, setImages] = useState([])
-  for (let i = 1; images.length < 16; i++) {
-    images.push(`images/img-${i}.jpeg`)
+  let images = []
+  
+  for (let i = 1; i < 16; i++) {
+      images.push(`images/img-${i}.jpeg`)
   }
-  const shuffle = () => {
-    var copy = [], n = images.length, i;
 
-    // While there remain elements to shuffle…
-    while (n) {
-
-      // Pick a remaining element…
-      i = Math.floor(Math.random() * images.length);
-
-      // If not already shuffled, move it to the new images.
-      if (i in images) {
-        copy.push(images[i]);
-        delete images[i];
-        n--;
-      }
-    }
-    return copy
-  }
-  useEffect(() => {
-    setImages(shuffle())
-  }, [])
-
+  images = images.sort(() => Math.random() - 0.5)
 
   return (
     <>
