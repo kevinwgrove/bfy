63b41c5ab5093e881ee8d59caa83a761f154b3f5
Kevin Grove <kevinwilsongrove@gmail.com>
2021-08-14 17:12:20 -0400

    Rolled commit back to slideshow version, commiting video play version.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
 import "./App.css";
-import BackgroundSlider from "react-background-slider";
+import "react-bootstrap";
+import {
+  IconButton
+} from "@material-ui/core"
+import { VolumeOff, VolumeUp } from "@material-ui/icons"
+import { useState } from "react";
 
 function App() {
-  let images = []
-  
-  for (let i = 1; i < 16; i++) {
-      images.push(`images/img-${i}.jpeg`)
-  }
+  const [muted, setMuted] = useState(true)
 
-  images = images.sort(() => Math.random() - 0.5)
+  const toggleSound = () => {
+    const element = document.getElementById('video')
+    element.muted = !element.muted
+    setMuted(!muted)
+  }
 
   return (
     <>
-        <BackgroundSlider 
-          id="background"
-          images={images}
-          duration={8}
-          transition={2}
-        />
-          <div id="app-container"></div>
+      <div id="video-wrapper">
+        <video id="video" preload autoPlay poster loop muted>
+          <source src="videos/northern_soul_lq.mp4" type="video/mp4" />
+        </video>
+      </div>
+      <div 
+        id="overlay-wrapper"
+      >
+        <div
+          id="menu-bar"
+        >
+        </div>
+        <div
+          id="sound-button-container"
+        >
+          {
+            muted
+            ?
+            <IconButton
+              onClick={toggleSound}
+            >
+              <VolumeOff 
+                style={{
+                  color: 'white'
+                }}
+              />
+            </IconButton>
+            :
+            <IconButton
+              onClick={toggleSound}
+            >
+              <VolumeUp 
+                style={{
+                  color: 'white'
+                }}
+              />
+            </IconButton>
+          }
+        </div>
+      </div>
     </>
   );
 }
